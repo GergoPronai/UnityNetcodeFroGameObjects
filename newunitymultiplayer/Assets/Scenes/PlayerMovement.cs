cd10acf011413af7a9aecebd6bd6048bab0fad9d
Bailley <pople.bailley@gmail.com>
2022-11-02 20:36:09 +0000

    removed steam functionality in favour of a different approach

--- newunitymultiplayer/Assets/Scenes/PlayerMovement.cs
+++ newunitymultiplayer/Assets/Scenes/PlayerMovement.cs
@@ -49,9 +49,9 @@ public class PlayerMovement : NetworkBehaviour
     }
     
 }
-/*public static class Helpers
+public static class Helpers
 {
     private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
     public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
-}*/
+}
 
