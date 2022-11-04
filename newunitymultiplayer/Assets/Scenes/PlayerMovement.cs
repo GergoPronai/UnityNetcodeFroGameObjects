deab823fd9da6e103da645199e8697dabb62faf4
Bailley <pople.bailley@gmail.com>
2022-11-04 01:00:12 +0000

    can connect via ip address

--- newunitymultiplayer/Assets/Scenes/PlayerMovement.cs
+++ newunitymultiplayer/Assets/Scenes/PlayerMovement.cs
@@ -54,4 +54,3 @@ public static class Helpers
     private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
     public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
 }
-
