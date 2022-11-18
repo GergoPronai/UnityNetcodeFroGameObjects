cfe25f47e678bdb72fc123206adc08df1eaa733a
Bailley <pople.bailley@gmail.com>
2022-11-18 17:22:06 +0000

    Update PlayergameObjScript.cs

--- newunitymultiplayer/Assets/Scenes/PlayergameObjScript.cs
+++ newunitymultiplayer/Assets/Scenes/PlayergameObjScript.cs
@@ -21,14 +21,12 @@ public class PlayergameObjScript : NetworkBehaviour
     public int _Charchosen;
     public CharacterChoices CharChosen;
     public ulong clientID;
-
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
 
         PlayerName = NetworkManagerUiMain.instance.PlayerName;
-        
-
+       
     }
     public override void OnNetworkDespawn()
     {
