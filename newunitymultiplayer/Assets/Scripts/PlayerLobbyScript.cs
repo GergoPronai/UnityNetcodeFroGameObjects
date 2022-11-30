db698b2b5fef6b313ae6ad11f44bbe687d9e04a5
Bailley <pople.bailley@gmail.com>
2022-11-30 00:45:38 +0000

    found a way to add lobby player cards to show moveset that players have chosen and possibly synchronise it across all players

--- newunitymultiplayer/Assets/Scripts/PlayerLobbyScript.cs
+++ newunitymultiplayer/Assets/Scripts/PlayerLobbyScript.cs
@@ -7,7 +7,6 @@ public class PlayerLobbyScript : MonoBehaviour
     [Header("Player Stuff")]
     [Header("UI Stuff")]
     public TMPro.TextMeshProUGUI PlayerNameText;
-    public TMPro.TextMeshProUGUI PlayerCharacterName;
     [SerializeField] Sprite[] CharacterImages;
     [SerializeField] private UnityEngine.UI.Image CharacterImage;
     public GameObject instantiateObj;
@@ -17,19 +16,15 @@ public class PlayerLobbyScript : MonoBehaviour
         switch (CharChosen)
         {
             case CharacterChoices.Barbarian:
-                PlayerCharacterName.text = "Barbarian";
                 CharacterImage.sprite =CharacterImages[0];
                 break;
             case CharacterChoices.Knight:
-                PlayerCharacterName.text = "Knight";
                 CharacterImage.sprite = CharacterImages[1];
                 break;
             case CharacterChoices.Mage:
-                PlayerCharacterName.text = "Mage";
                 CharacterImage.sprite = CharacterImages[2];
                 break;
             case CharacterChoices.Rogue:
-                PlayerCharacterName.text = "Rogue";
                 CharacterImage.sprite = CharacterImages[3];
                 break;
         }
