126dcfd6c6a4c3bf95e897ca9693c97758465d7a
Bailley <pople.bailley@gmail.com>
2022-12-21 02:14:57 +0000

    altered environment, adding a little roof around the walls to cover clipping issues

--- newunitymultiplayer/Assets/Dungeon/DungeonGen/Scripts/GameRoom.cs
+++ newunitymultiplayer/Assets/Dungeon/DungeonGen/Scripts/GameRoom.cs
@@ -23,48 +23,56 @@ public class GameRoom : MonoBehaviour {
 			doorWest.transform.GetChild(0).gameObject.SetActive(true);
 			doorWest.transform.GetChild(1).gameObject.SetActive(false);
 			doorWest.transform.GetChild(2).gameObject.SetActive(false);
+			doorWest.transform.GetChild(3).gameObject.SetActive(false);
 		}
 		else
 		{
 			doorWest.transform.GetChild(0).gameObject.SetActive(false);
 			doorWest.transform.GetChild(1).gameObject.SetActive(true);
 			doorWest.transform.GetChild(2).gameObject.SetActive(true);
+			doorWest.transform.GetChild(3).gameObject.SetActive(true);
 		}
 		if (room.IsConnectedTo(room.GetRight()))
 		{
 			doorEast.transform.GetChild(0).gameObject.SetActive(true);
 			doorEast.transform.GetChild(1).gameObject.SetActive(false);
 			doorEast.transform.GetChild(2).gameObject.SetActive(false);
+			doorEast.transform.GetChild(3).gameObject.SetActive(false);
 		}
 		else
 		{
 			doorEast.transform.GetChild(0).gameObject.SetActive(false);
 			doorEast.transform.GetChild(1).gameObject.SetActive(true);
 			doorEast.transform.GetChild(2).gameObject.SetActive(true);
+			doorEast.transform.GetChild(3).gameObject.SetActive(true);
 		}
 		if (room.IsConnectedTo(room.GetTop()))
 		{
 			doorNorth.transform.GetChild(0).gameObject.SetActive(true);
 			doorNorth.transform.GetChild(1).gameObject.SetActive(false);
 			doorNorth.transform.GetChild(2).gameObject.SetActive(false);
+			doorNorth.transform.GetChild(3).gameObject.SetActive(false);
 		}
 		else
 		{
 			doorNorth.transform.GetChild(0).gameObject.SetActive(false);
 			doorNorth.transform.GetChild(1).gameObject.SetActive(true);
 			doorNorth.transform.GetChild(2).gameObject.SetActive(true);
+			doorNorth.transform.GetChild(3).gameObject.SetActive(true);
 		}
 		if (room.IsConnectedTo(room.GetBottom()))
 		{
 			doorSouth.transform.GetChild(0).gameObject.SetActive(true);
 			doorSouth.transform.GetChild(1).gameObject.SetActive(false);
 			doorSouth.transform.GetChild(2).gameObject.SetActive(false);
+			doorSouth.transform.GetChild(3).gameObject.SetActive(false);
 		}
 		else
 		{
 			doorSouth.transform.GetChild(0).gameObject.SetActive(false);
 			doorSouth.transform.GetChild(1).gameObject.SetActive(true);
 			doorSouth.transform.GetChild(2).gameObject.SetActive(true);
+			doorSouth.transform.GetChild(3).gameObject.SetActive(true);
 		}
 		setupRoom();
 	}
