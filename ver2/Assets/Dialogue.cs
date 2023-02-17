390d1c5945ff43b96216ebd27bc1254e63b2e9a7
Bailley <pople.bailley@gmail.com>
2023-02-17 00:46:50 +0000

    added dialogue box system

---
+++ ver2/Assets/Dialogue.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Dialogue : MonoBehaviour
+{
+
+    public TextMeshProUGUI textMeshComponent;
+    public string[] lines;
+    public float text_Speed;
+    private int index;
+
+    void Start()
+    {
+        textMeshComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textMeshComponent.text += c;
+            yield return new WaitForSeconds(text_Speed);
+        }
+    }
+    void NextLine()
+    {
+        if (index< lines.Length-1)
+        {
+            index++;
+            textMeshComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    public void OnMouseClickedButton()
+    {
+        if (textMeshComponent.text == lines[index])
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            textMeshComponent.text = lines[index];
+        }
+    }
+}
